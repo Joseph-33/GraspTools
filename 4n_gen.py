11eb6ddc2f9546fb25922dba4c452659127cc217
Joseph-33 <bb017693-ab7a-4606-81c8-3fe84a078b00@aleeas.com>
2023-12-09 17:00:12 +0100

    Make 4n better

--- 4n_gen.py
+++ 4n_gen.py
@@ -3,8 +3,38 @@ import pdb
 import re
 bp = breakpoint
 
-file_name = "rcsfmr_even_99_5d5f_PeelEdited.inp"
-out_name = "rcsfmr_out.inp"
+file_name = "rcsf.inp"
+out_name = "rcsf.out"
+
+replace_csf = r"  5d-( 4)  5d ( 6)  5f-( 6)  5f ( 8)"
+
+print("Welcome to the core subshell generator.\nThis program will add full core subshells to your csf file.")
+print("The current csf to be added are '{}'".format(replace_csf))
+filans = input("The input and output files are:\nInput: {}\nOutput: {}\nWould you like to change them? (y/n)\n".format(file_name,out_name))
+if filans.lower() == 'y':
+    file_name = input("Type input name: \n")
+    out_name = input("Type output name: \n")
+ans = input("Would you like option 1 or option 2?: \n")
+
+if not ans.isdigit():
+    print("Choose an integer")
+    exit()
+elif int(ans) == 1:
+    opt = 1
+    print("Option 1 selected")
+elif int(ans) == 2:
+    opt = 2
+    print("Option 2 selected")
+else:
+    print("Choose either 1 or 2")
+    exit()
+
+
+
+pattern_csf = r"^(\s\s6s\s\()"
+pattern_ang = r"^(\s\s\s)"
+replace_ang = " " * len(replace_csf)
+patterns = [pattern_csf, replace_csf, pattern_ang, pattern_ang]
 
 def file_selector():
     file_names = [i for i in listdir()]
@@ -19,27 +49,41 @@ def file_selector():
         file_name = file_names[int(valuechoice)-1]
     return file_name
 
+
+def option1(lines, patterns):
+    pattern_csf, replace_csf, pattern_ang, replace_ang = patterns
+    result = []
+    for i in range(len(lines)):
+        if i < 5:
+            continue
+
+        subs_csf = re.sub(pattern_csf, replace_csf + r"\1", lines[i])
+        subs_ang = re.sub(pattern_ang, replace_ang + r"\1", subs_csf)
+
+        result.append(subs_ang)
+    return result
+
+def option2(lines, patterns):
+    replace_csf, replace_ang = patterns
+    result=[]
+    for i in range(len(lines)):
+        if i < 5:
+            continue
+
+        if "(" in lines[i]: # For the CSF part
+            subs = replace_csf + lines[i]
+        elif "*" not in line[i]: # For the angular part
+            subs = replace_ang + lines[i]
+        else:
+            subs = lines[i]
+
+        result.append(subs)
+
+    return result
+
+
 with open(file_name,'r') as fil:
     lines = fil.readlines()
-
-pattern_csf = r"^(\s\s6s\s\()"
-replace_csf = r"  5d-( 4)  5d ( 6)  5f-( 6)  5f ( 8)"
-
-pattern_ang = r"^(\s\s\s)"
-replace_ang = " " * len(replace_csf)
-
-result = []
-for i in range(len(lines)):
-    if i < 5:
-        continue
-
-    subs_csf = re.sub(pattern_csf, replace_csf + r"\1", lines[i])
-    subs_ang = re.sub(pattern_ang, replace_ang + r"\1", subs_csf)
-
-#    if subs == lines[i]:
-#        subs = " " * repl_len + subs
-
-    result.append(subs_ang)
 
 out_str = "".join(result)
 csf_str = "".join(lines[:5])
